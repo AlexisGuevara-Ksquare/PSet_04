0621a266b3fe16532b522ef5ea758b10fd3f304f
AlexisGuevara-ITK <alexis.guevara@itksquare.edu.mx>
2023-03-28 17:04:48 -0700

    Responsive design: complete

---
+++ prob_1/README.md
@@ -0,0 +1,5 @@
+# Problem 1
+
+GitHub Pages [press here](https://alexisguevara-ksquare.github.io/PSet_04/prob_1/)
+
+![Drag Racing](./img/imgReadMe.png)
--- prob_1/prob_1.js
+++ prob_1/prob_1.js
@@ -2,27 +2,33 @@
 // Get card Container
 const cardContainer = document.querySelector('.cardContainer');
 
+// Array for starage the objects from the data API
 let elementsLive = [];
 
 // API
 let url = 'https://gist.githubusercontent.com/carmandomx/b27e23332eda1d061feb3cdada26afc0/raw/438d33407442d2abbf605e87336f48a83ccff3f5/data.json';
 
+// Main function to get data from API
 let main = async() => {
     const response = await fetch(url);
     const data = await response.json();
 
+    // Fill array with objects from API
     data.forEach(element => {
         elementsLive.push(element);
     });
 
+    // Send array to the function that will render the cards in the HTML
     createCards(elementsLive);
 }
 
 let createCards = (cardData) => {
+    // Counter to change icon
     let count = 0;
 
     cardData.forEach(item => {
 
+        // Trim titles that have blank space so it can be added as class
         let triming = item.title;
         let newtrim = triming.split(' ').join('');
         
@@ -43,7 +49,10 @@ let createCards = (cardData) => {
             <p class="cardText lastTime">Last week: ${item.timeframes.weekly.previous}hrs</p>
         </div>
         `;
+        // Send the render to inside the cardContainer Div
         cardContainer.append(div);
+
+        // Increase counter
         count = count + 1;
     });
 }
